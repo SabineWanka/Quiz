5fc2b5d3604c8a2009fad8f00e2c7526b30ede1d
SabineWanka <sabinewanka@yahoo.de>
2019-10-14 23:58:07 +0200

    debugging

--- script.js
+++ script.js
@@ -1,34 +1,85 @@
-var questions = document.querySelector("#questions")
-var startButton = document.querySelector("#startButton")
-var submitButton = document.querySelector("#submitButton")
-var clearButton = document.querySelector("#clearButton")
-var backButton = document.querySelector("#backButton")
+var questions = document.querySelector("#questions");
+var startButton = document.querySelector("#startButton");
+var submitButton = document.querySelector("#submitButton");
+var clearButton = document.querySelector("#clearButton");
+var backButton = document.querySelector("#backButton");
+var final = document.querySelector("#final");
+var highScores = document.querySelector("#highScores");
+var initialInput = document.querySelector("#initial");
 var score = 0
 var right = 0
 var wrong = 0
 
+var secondsLeft = 75000;
 
-var secondsLeft = 75;
+getHighScores();
 
 for (var i = 0; i < questions.length; i++) {
     var response = questions[i]
-    if (response === question[i.answer]) {
+    if (response === questions[i.answer]) {
         alert("Right");
         score++;
+    }
     else {
-            alert("Wrong!");
-            secondsLeft--;
-        }
+        alert("Wrong!");
+        secondsLeft--;
     }
 }
-alert("you got" + score + "/" + question.length);
 
-function setTime() {
-    var timerInterval = setInterval(timeFn, 75000);
+alert("you got" + score + "/" + questions.length);
+
+function startButton() {
+    var questions = document.querySelector("#questions")
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
+
+function submitButton() {
+    var final = document.querySelector("#final")
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
+
+function backButton() {
+    var highScores = document.querySelector("#highScores")
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
+
+}
+
+function clearButton() {
+    var highScores = document.querySelector("#highScores")
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
+
+}
+
+function startTimer() {
+    setTime();
+
+    interval = setInterval(function () {
+        secondsElapsed++;
+        renderTime();
+        75000);
 }
 function timeFn() {
     secondsLeft--;
-
     if (secondsLeft === 0) {
         clearInterval(timerInterval);
     }
@@ -38,10 +89,22 @@ function stopTimer() {
     setTime();
     renderTime();
 }
+
+function renderhighScores() {
+    var highScores = localStorage.getItem("highScores")
+
+}
+var highScores = document.querySelector("#highScores").value
+
+localStorage.setItem("highScores", highScores)
+
+
+
 startButton.addEventListener("click", startTimer);
 submitButton.addEventListener("click", submitQuiz);
-backButton.addEventListener("click");
-clearButton.addEventListener("click")
+backButton.addEventListener("click", goBack);
+clearButton.addEventListener("click", clearHighscores);
+
 
 
 
